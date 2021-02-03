b16017473abfe9282cb13cfef69b93002bc98ab1
Shintaro Iwasaki <siwasaki@anl.gov>
2021-02-03 13:52:06 -0600

    mpi/pt2pt/osu_latency_mt: add a pthreads wrapper

--- mpi/pt2pt/osu_latency_mt.c
+++ mpi/pt2pt/osu_latency_mt.c
@@ -11,12 +11,80 @@
 
 #include <osu_util_mpi.h>
 
-pthread_mutex_t finished_size_mutex;
-pthread_cond_t finished_size_cond;
-pthread_mutex_t finished_size_sender_mutex;
-pthread_cond_t finished_size_sender_cond;
+typedef pthread_mutex_t thread_mutex_t;
+typedef pthread_cond_t thread_cond_t;
+typedef pthread_barrier_t thread_barrier_t;
+typedef pthread_t thread_t;
 
-pthread_barrier_t sender_barrier;
+void init_thread()
+{
+    ;
+}
+void finalize_thread()
+{
+    ;
+}
+
+int thread_mutex_init(thread_mutex_t *mutex, const void *attr)
+{
+    return pthread_mutex_init(mutex, (const pthread_mutexattr_t *)attr);
+}
+
+int thread_cond_init(thread_cond_t *cond, const void *attr)
+{
+    return pthread_cond_init(cond, (const pthread_condattr_t *)attr);
+}
+
+int thread_barrier_init(thread_barrier_t *barrier, const void *attr,
+                        unsigned count)
+{
+    return pthread_barrier_init(barrier, (const pthread_barrierattr_t *)attr,
+                                count);
+}
+
+int thread_create(thread_t *thread, const void *attr,
+                  void *(*start_routine)(void *), void *arg)
+{
+    return pthread_create(thread, (const pthread_attr_t *)attr, start_routine,
+                          arg);
+}
+
+int thread_join(thread_t thread, void **retval)
+{
+    return pthread_join(thread, retval);
+}
+
+int thread_mutex_lock(thread_mutex_t *mutex)
+{
+    return pthread_mutex_lock(mutex);
+}
+
+int thread_mutex_unlock(thread_mutex_t *mutex)
+{
+    return pthread_mutex_unlock(mutex);
+}
+
+int thread_cond_broadcast(thread_cond_t *cond)
+{
+    return pthread_cond_broadcast(cond);
+}
+
+int thread_cond_wait(thread_cond_t *cond, thread_mutex_t *mutex)
+{
+    return pthread_cond_wait(cond, mutex);
+}
+
+int thread_barrier_wait(thread_barrier_t *barrier)
+{
+    return pthread_barrier_wait(barrier);
+}
+
+thread_mutex_t finished_size_mutex;
+thread_cond_t finished_size_cond;
+thread_mutex_t finished_size_sender_mutex;
+thread_cond_t finished_size_sender_cond;
+
+thread_barrier_t sender_barrier;
 
 double t_start = 0, t_end = 0;
 
@@ -33,16 +101,17 @@ void *recv_thread(void *arg);
 
 int main(int argc, char *argv[])
 {
+    init_thread();
     int numprocs = 0, provided = 0, myid = 0, err = 0;
     int i = 0;
     int po_ret = 0;
-    pthread_t sr_threads[MAX_NUM_THREADS];
+    thread_t sr_threads[MAX_NUM_THREADS];
     thread_tag_t tags[MAX_NUM_THREADS];
 
-    pthread_mutex_init(&finished_size_mutex, NULL);
-    pthread_cond_init(&finished_size_cond, NULL);
-    pthread_mutex_init(&finished_size_sender_mutex, NULL);
-    pthread_cond_init(&finished_size_sender_cond, NULL);
+    thread_mutex_init(&finished_size_mutex, NULL);
+    thread_cond_init(&finished_size_cond, NULL);
+    thread_mutex_init(&finished_size_sender_mutex, NULL);
+    thread_cond_init(&finished_size_sender_cond, NULL);
 
     options.bench = PT2PT;
     options.subtype = LAT_MT;
@@ -137,7 +206,7 @@ int main(int argc, char *argv[])
         num_threads_sender = options.sender_thread;
     }
 
-    pthread_barrier_init(&sender_barrier, NULL, num_threads_sender);
+    thread_barrier_init(&sender_barrier, NULL, num_threads_sender);
 
     if (myid == 0) {
         printf("# Number of Sender threads: %d \n# Number of Receiver threads: "
@@ -150,23 +219,24 @@ int main(int argc, char *argv[])
 
         for (i = 0; i < num_threads_sender; i++) {
             tags[i].id = i;
-            pthread_create(&sr_threads[i], NULL, send_thread, &tags[i]);
+            thread_create(&sr_threads[i], NULL, send_thread, &tags[i]);
         }
         for (i = 0; i < num_threads_sender; i++) {
-            pthread_join(sr_threads[i], NULL);
+            thread_join(sr_threads[i], NULL);
         }
     } else {
         for (i = 0; i < options.num_threads; i++) {
             tags[i].id = i;
-            pthread_create(&sr_threads[i], NULL, recv_thread, &tags[i]);
+            thread_create(&sr_threads[i], NULL, recv_thread, &tags[i]);
         }
 
         for (i = 0; i < options.num_threads; i++) {
-            pthread_join(sr_threads[i], NULL);
+            thread_join(sr_threads[i], NULL);
         }
     }
 
     MPI_CHECK(MPI_Finalize());
+    finalize_thread();
 
     return EXIT_SUCCESS;
 }
@@ -200,22 +270,22 @@ void *recv_thread(void *arg)
 
     for (size = options.min_message_size, iter = 0;
          size <= options.max_message_size; size = (size ? size * 2 : 1)) {
-        pthread_mutex_lock(&finished_size_mutex);
+        thread_mutex_lock(&finished_size_mutex);
 
         if (finished_size == options.num_threads) {
             MPI_CHECK(MPI_Barrier(MPI_COMM_WORLD));
 
             finished_size = 1;
 
-            pthread_mutex_unlock(&finished_size_mutex);
-            pthread_cond_broadcast(&finished_size_cond);
+            thread_mutex_unlock(&finished_size_mutex);
+            thread_cond_broadcast(&finished_size_cond);
         }
 
         else {
             finished_size++;
 
-            pthread_cond_wait(&finished_size_cond, &finished_size_mutex);
-            pthread_mutex_unlock(&finished_size_mutex);
+            thread_cond_wait(&finished_size_cond, &finished_size_mutex);
+            thread_mutex_unlock(&finished_size_mutex);
         }
 
         if (size > LARGE_MESSAGE_SIZE) {
@@ -278,21 +348,21 @@ void *send_thread(void *arg)
 
     for (size = options.min_message_size, iter = 0;
          size <= options.max_message_size; size = (size ? size * 2 : 1)) {
-        pthread_mutex_lock(&finished_size_sender_mutex);
+        thread_mutex_lock(&finished_size_sender_mutex);
 
         if (finished_size_sender == num_threads_sender) {
             MPI_CHECK(MPI_Barrier(MPI_COMM_WORLD));
 
             finished_size_sender = 1;
 
-            pthread_mutex_unlock(&finished_size_sender_mutex);
-            pthread_cond_broadcast(&finished_size_sender_cond);
+            thread_mutex_unlock(&finished_size_sender_mutex);
+            thread_cond_broadcast(&finished_size_sender_cond);
         } else {
             finished_size_sender++;
 
-            pthread_cond_wait(&finished_size_sender_cond,
-                              &finished_size_sender_mutex);
-            pthread_mutex_unlock(&finished_size_sender_mutex);
+            thread_cond_wait(&finished_size_sender_cond,
+                             &finished_size_sender_mutex);
+            thread_mutex_unlock(&finished_size_sender_mutex);
         }
 
         if (size > LARGE_MESSAGE_SIZE) {
@@ -325,7 +395,7 @@ void *send_thread(void *arg)
             }
         }
 
-        pthread_barrier_wait(&sender_barrier);
+        thread_barrier_wait(&sender_barrier);
         if (flag_print == 1) {
             t_end = MPI_Wtime();
             t = t_end - t_start;
